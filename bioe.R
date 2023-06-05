009788466f85341a34c7134771497ee68b2e9e04
David Nadler <100381386+nadlerphd@users.noreply.github.com>
2023-06-05 14:08:31 -0400

    Delete bioe.R

--- bioe.R
+++
@@ -1,7 +0,0 @@
-df3 <- df2 %>% 
-  filter(`Water Use (All Water Sources) (kgal)` < 500000)
-
-range(df3$`Water Use (All Water Sources) (kgal)`)
-
-ggplot(data = df3) +
-  geom_histogram(aes(x = `Water Use (All Water Sources) (kgal)`))
